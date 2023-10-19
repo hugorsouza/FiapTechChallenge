abcb7ffe806169065b67118b39a8da8197c46edc
Lucas Rodrigues <rs.lucas1@gmail.com>
2023-10-19 14:26:24 -0300

    Envio IServices

---
+++ Ecommerce.Domain/Services/IFabricanteService.cs
@@ -0,0 +1,15 @@
+﻿using Ecommerce.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Domain.Services
+{
+    public interface IFabricanteService : IService<Fabricante>
+    {
+
+
+    }
+}
