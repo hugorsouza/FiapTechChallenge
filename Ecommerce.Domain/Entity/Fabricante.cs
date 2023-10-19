f4df4ec4530eaca0529974cf6c67387c24f6ca24
Lucas Rodrigues <rs.lucas1@gmail.com>
2023-10-19 14:23:27 -0300

    ajuste domain

---
+++ Ecommerce.Domain/Entity/Fabricante.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Domain.Entity
+{
+    public class Fabricante : Entidade
+    {
+        public string CNPJ { get; set; }
+        public Endereco Endereco { get; set; }
+
+    }
+}
---
+++ Ecommerce.Domain/Entity/Fornecedor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Domain.Entity
+{
+    public class Fornecedor : Entidade
+    {
+        public string CNPJ { get; set; }
+        public Endereco Endereco { get; set; }
+
+
+    }
+}
